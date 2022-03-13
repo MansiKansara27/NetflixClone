5c00c0c573a50768d062d7cd64ec0771bb252053
AJTech1968 <40666123+AJTech1968@users.noreply.github.com>
2022-03-13 17:48:53 +0530

    chat integration done

--- src/Nav.js
+++ src/Nav.js
@@ -1,32 +1,36 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.css';
+import { Link } from "react-router-dom"
 
 function Nav() {
 	const [show, handleShow] = useState(false);
 
 	useEffect(() => {
 		window.addEventListener("scroll", () => {
-			if(window.scrollY > 100) {
+			if (window.scrollY > 100) {
 				handleShow(true);
 			} else handleShow(false);
 		});
-		return() => {
+		return () => {
 			window.removeEventListener("scroll");
 		};
 	}, []);
 
-	return(
+	return (
 		<div className={`nav ${show && "nav_black"}`}>
-			<img 
-				className = "nav_logo"
-				src = "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
-				alt = "Netflix Logo"
+			<img
+				className="nav_logo"
+				src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+				alt="Netflix Logo"
 			/>
-			<img 
-				className = "nav_avatar"
-				src = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-				alt = "Netflix Logo"	
-			/>
+			<div className="chat">
+				<Link to="/chat"><h3>Chat</h3></Link>
+				<img
+					className="nav_avatar1"
+					src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+					alt="Netflix Logo"
+				/>
+			</div>
 		</div>
 	)
 }
--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,23 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC2xByXBZJBuLghrxuAwrXu_o2QlWtsyg8",
+//   authDomain: "netflix-cb9e6.firebaseapp.com",
+//   projectId: "netflix-cb9e6",
+//   storageBucket: "netflix-cb9e6.appspot.com",
+//   messagingSenderId: "298130629857",
+//   appId: "1:298130629857:web:17e80243fe8bf7ad00c3a0"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC2xByXBZJBuLghrxuAwrXu_o2QlWtsyg8",
-  authDomain: "netflix-cb9e6.firebaseapp.com",
-  projectId: "netflix-cb9e6",
-  storageBucket: "netflix-cb9e6.appspot.com",
-  messagingSenderId: "298130629857",
-  appId: "1:298130629857:web:17e80243fe8bf7ad00c3a0"
-};
+  apiKey: "AIzaSyCAUOFl0SZvpaRVB4F-0YiETcRngSnqRo8",
+  authDomain: "hirethebest-6d267.firebaseapp.com",
+  projectId: "hirethebest-6d267",
+  storageBucket: "hirethebest-6d267.appspot.com",
+  messagingSenderId: "320564176285",
+  appId: "1:320564176285:web:e9849c99e1a6d024abbb74"
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
